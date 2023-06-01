948b894f404a81cd477e6dbbf643c9b18d9527b8
BrunoLucass <bruno.2020109475@unicap.br>
2023-05-31 23:32:10 -0300

    main

---
+++ README.md
@@ -0,0 +1,5 @@
+# javascript-interpreter-example
+
+Javascript Interpreter Example Code
+
+This is an implementation of an language in pure javascript purpose of this repository is to demonstrate how such an interpreter can be built.
---
+++ index.js
@@ -0,0 +1,54 @@
+// Code which we want to parse
+const code = `
+firstName = 'John';
+lastName = 'Smith';
+age = 50;
+
+print('Entered name: ' + firstName + ', ' + lastName);
+
+if (age > 40) {
+    if (age > 45) {
+        print('Age is above 45');
+    }
+    print('Welcome, ' + firstName + ' you are ' + age + ' old.');
+}
+`;
+
+// Import the lexer
+const analyseCode = require('./lexer-analyser');
+
+// Run the lexer
+const tokens = analyseCode(code);
+
+// Import the parser
+const parseTokens = require('./parser-analyser');
+
+// Run the parser
+const statements = parseTokens(tokens);
+
+
+// Import the interpreter
+const interpret = require('./interpreter');
+
+console.log('Code we ran:');
+console.log(code);
+console.log("--------------------------------------------------------")
+
+const vm = {
+    variables: {},
+    functions: {
+        print(message) { // We add a print function so that we can call a function from our code.
+            console.log('MESSAGE:', message); 
+        },
+        pow(x, y) { // We also add a function which returns something for an expression.
+            return Math.pow(x, y);
+        }
+    }
+};
+// Interpret the statements and return last result.
+const result = interpret(statements, vm);
+
+// And finally we output the result
+console.log('Result:')
+console.dir(result, {depth: null});
+console.log('Final VM State:', vm);
