1f98c4ad9009942c0e776125aae3303b2a02f44e
Hugo Cortez <hugocortezprog@gmail.com>
2023-06-01 21:46:25 -0300

    change code

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ const parseTokens = require('./analysers/parser-analyser');
 const interpret = require('./analysers/interpreter');
 
 const code = `
-let firstName = "John"; // let a = 4
-// testando comentarios
+var x = 10;
+let y = 5;
+const z = 20;
+
+var sum = x + y;
+var diff = x - y;
+var prod = x * y;
+var quot = x / y;
+
+var isSumGreaterThanDiff = sum > diff;
+var isProdLessThanQuot = prod < quot;
+var isSumEqualtoZ = sum === z;
+var isXnotEqualToY = x !== y;
+var areBothConditionsTrue = isSumGreaterThanDiff && isProdLessThanQuot;
+var areEitherConditionsTrue = isSumEqualtoZ || isXnotEqualToY;
+
+if (areBothConditionsTrue) {
+    print("Both conditions are true");
+}
+
+//Esse é um exemplo de comentario
+
+
 `;
 
 const tokens = analyseCode(code);
